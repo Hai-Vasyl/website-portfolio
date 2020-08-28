5098bff12834470de631f8c5a57ecb1f3cf2d6db
Vasyl Hai <hai.vasyl20@gmail.com>
2020-08-28 15:31:08 +0300

    Created basic Contacts page with mail send engine

--- client/src/pages/Contacts.js
+++ client/src/pages/Contacts.js
@@ -1,9 +1,147 @@
-import React from "react"
+import React, { useState } from "react"
+import axios from "axios"
+import { RiMailSendLine } from "react-icons/ri"
+import { FaLinkedinIn, FaGithub } from "react-icons/fa"
+import { ImWarning } from "react-icons/im"
+import contactsImage from "../images/undraw_envelope_n8lc.svg"
 
 function ContactsSection() {
+  const [form, setForm] = useState([
+    { param: "name", name: "Contact name", value: "", msg: "" },
+    { param: "email", name: "Email address", value: "", msg: "" },
+    { param: "message", name: "Message", value: "", msg: "" },
+  ])
+  const [load, setLoad] = useState(false)
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault()
+      let isEmptyField = false
+
+      setForm(
+        form.map((item) => {
+          if (!item.value) {
+            item.msg = "Fill this field!"
+            isEmptyField = true
+          }
+          return item
+        })
+      )
+
+      if (isEmptyField) {
+        return
+      }
+
+      const [name, email, message] = form
+      const res = await axios({
+        url: "/post-email",
+        method: "post",
+        data: { name: name.value, email: email.value, message: message.value },
+      })
+      console.log(res.data)
+    } catch (error) {
+      const { errors } = error.response.data
+      setForm(
+        form.map((item) => {
+          errors.forEach((err) => {
+            if (item.param === err.param) {
+              item.msg = err.msg
+            }
+          })
+          return item
+        })
+      )
+    }
+  }
+
+  const handleChange = (e) => {
+    setForm(
+      form.map((item) => {
+        if (item.param === e.target.name) {
+          item.msg = ""
+          item.value = e.target.value
+        }
+        return item
+      })
+    )
+  }
+
+  const fields = form.map((item) => {
+    return (
+      <label key={item.param} className='field'>
+        <span className='field__name'>{item.name}</span>
+        {item.param === "message" ? (
+          <textarea
+            className={`field__textarea field__input ${
+              item.msg && "field__input--error"
+            }`}
+            name={item.param}
+            value={item.value}
+            onChange={handleChange}
+          ></textarea>
+        ) : (
+          <input
+            className={`field__input ${item.msg && "field__input--error"}`}
+            name={item.param}
+            type={item.param === "email" ? "email" : "text"}
+            value={item.value}
+            onChange={handleChange}
+            autoComplete='off'
+          />
+        )}
+        <span className={`field__msg ${item.msg && "field__msg--active"}`}>
+          <ImWarning className='field__msg-icon' />
+          <span className='field__msg-name'>{item.msg}</span>
+        </span>
+      </label>
+    )
+  })
+
   return (
-    <div>
-      <div>ContactsSection.js</div>
+    <div className='wrapper'>
+      <h2 className='title'>Contact me</h2>
+
+      <div className='contacts'>
+        <div className='form'>
+          <form onSubmit={handleSubmit} className='form__fields'>
+            {fields}
+            <button className='form__btn-handler'></button>
+          </form>
+          <div className='form__btns'>
+            <a
+              href='https://github.com/Hai-Vasyl'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='form__link'
+            >
+              <FaGithub className='form__icon' />
+            </a>
+            <a
+              href='https://www.linkedin.com/in/vasyl-hai-2819051b1/'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='form__link'
+            >
+              <FaLinkedinIn className='form__icon' />
+            </a>
+            <button
+              className='form__submit btn btn-primary'
+              onClick={handleSubmit}
+            >
+              <RiMailSendLine className='btn__icon' />
+              <span className='btn__name'>Submit</span>
+            </button>
+          </div>
+        </div>
+
+        <div className='contacts__image-side'>
+          <img
+            className='contacts__image'
+            src={contactsImage}
+            alt='contactsImg'
+          />
+        </div>
+      </div>
     </div>
   )
 }
--- index.js
+++ index.js
@@ -1,13 +1,73 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const { check, validationResult } = require("express-validator")
+const nodemailer = require("nodemailer")
 require("dotenv").config()
 const app = express()
 
 app.use(bodyParser.json())
 const PORT = process.env.PORT || 4000
 
-app.post("/mail", async (req, res) => {
-  res.json("mail posted")
-})
+app.post(
+  "/post-email",
+  [
+    check("name", "Name must contain at least 3 characters!").isLength({
+      min: 3,
+    }),
+    check("email", "Email is not correct!").isEmail(),
+    check("message", "Message must contain at least 5 characters!").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        return res.status(400).json(errors)
+      }
+
+      const { name, email, message } = req.body
+
+      const output = `
+        <h2>You have new contact message!</h2>
+        <h4>Details message</h4>
+        <ul>
+          <li>Name: ${name}</li>
+          <li>Email: ${email}</li>
+        </ul>
+        <h4>Message</h4>
+        <div>${message}</div>`
+
+      let transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.USER,
+          pass: process.env.PASS,
+        },
+        tls: {
+          rejectUnauthorized: false,
+        },
+      })
+
+      await transporter.sendMail(
+        {
+          from: process.env.USER,
+          to: "hai.vasyl20@gmail.com",
+          subject: "Contacts request",
+          text: "",
+          html: output,
+        },
+        (error, info) => {
+          if (error) {
+            return res.status(400).json(`Error posting email: ${error}`)
+          }
+          res.json("Email posted!")
+        }
+      )
+    } catch (error) {
+      res.json(`Error posting email: ${error.message}`)
+    }
+  }
+)
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
